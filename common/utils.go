c2f8beb2c2e7448b2fef395e5301c539c4c1c4f6
Igor Sirotin <sirotin@status.im>
2025-11-05 16:01:25 +0100

    chore: cleanup duplicated files

--- common/utils.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// LogOnPanic recovers from panic and logs it
-func LogOnPanic() {
-	// This is a dummy implementation for demonstration
-	// In a real application, this would handle panic recovery
-}
