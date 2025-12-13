69aec95597327e92b21db91e749bd1addc679fcb
Igor Sirotin <sirotin@status.im>
2025-12-13 13:03:30 +0000

    fix: remove logging in cmd

--- cmd/goroutine-defer-guard/main.go
+++ cmd/goroutine-defer-guard/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"golang.org/x/tools/go/analysis/singlechecker"
 
 	"github.com/status-im/goroutine-defer-guard/pkg/analyzer"
 )
 
 func main() {
-	logger := log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
-	a := analyzer.New(logger)
+	a := analyzer.New(nil)
 
 	// singlechecker runs the analyzer
 	singlechecker.Main(a)
